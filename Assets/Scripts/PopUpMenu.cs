870dd50df791271bd473716b39abc56c27cfc71e
uuwc <1051161242@qq.com>
2024-06-02 14:14:26 +0800

    add start menu

---
+++ Assets/Scripts/PopUpMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpMenu : MonoBehaviour
+{
+    public void Click()
+    {
+        gameObject.SetActive(true); 
+    }
+}
